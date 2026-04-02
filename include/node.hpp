95857ce702f01da12f37b4a1f34f33eb26f4c486
pranavmaske03 <pranavbmaske@gmail.com>
2026-04-02 21:52:08 +0530

    add: node.hpp — node_s, node_d, node_b structs inside namespace cds

---
+++ include/node.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace cds {
+    template <typename T>
+    struct node_s {
+        T data;
+        node_s* next;
+        explicit node_s(const T& val) : data(val), next(nullptr) {}
+    };
+
+    template <typename T>
+    struct node_d {
+        T data;
+        node_d* next;
+        node_d* prev;
+        explicit node_d(const T& val) : data(val), next(nullptr), prev(nullptr) {}
+    };
+
+    template <typename T>
+    struct node_b {
+        T data;
+        node_b* left;
+        node_b* right;
+        explicit node_b(const T& val) : data(val), left(nullptr), right(nullptr) {}
+    };
+}
